f510c722c32bae8504ae3ad83bf4269cc5724b0a
seolyeong333 <33qqpp@naver.com>
2025-04-08 12:42:33 +0900

    security folder

--- SpringBootEx/src/main/java/security/MemberMapper.java
+++ SpringBootEx/src/main/java/security/MemberMapper.java
@@ -4,9 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
 
+import logon.LogonDataBean;
+
 @Mapper
 public interface MemberMapper {
-	// mb/members.xml 과 연동 - security로 따로 만들다가 들고온거라 저희의 member.xml과 크게 다르지는 않지만 이름 때문에 꼭 필요해서 넣어놓았어요
 	public int insertMember( LogonDataBean dto );
 	public int check( String userId );
 	public int check( String userId, String passwd );
